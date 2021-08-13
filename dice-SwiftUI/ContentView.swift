54c009bf7312be2852871413f25afd6a8d72474f
mustafademiroz <83066615+mustafademiroz@users.noreply.github.com>
2021-08-13 18:28:29 +0300

    First Commit

--- dice-SwiftUI/ContentView.swift
+++ dice-SwiftUI/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 6
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background").resizable().edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack() {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }.padding()
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }, label: {
+                    Text("Roll").font(.system(size: 50)).bold().foregroundColor(.white).padding(.horizontal)
+                }).background(Color.red)
+            }
+            
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)").resizable().aspectRatio(1, contentMode: .fit).padding()
     }
 }
